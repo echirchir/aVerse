939717ab42701d8f510a12b8955d3582cffae282
Elisha <elisha.java@gmail.com>
2016-09-20 05:57:36 -0500

    more code to show authors

---
+++ app/src/main/java/com/simpledeveloper/averse/adapters/AuthorAdapter.java
@@ -0,0 +1,23 @@
+package com.simpledeveloper.averse.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class AuthorAdapter extends RecyclerView.Adapter<AuthorViewHolder>{
+
+    @Override
+    public AuthorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(AuthorViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/simpledeveloper/averse/adapters/AuthorViewHolder.java
@@ -0,0 +1,11 @@
+package com.simpledeveloper.averse.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+public class AuthorViewHolder extends RecyclerView.ViewHolder {
+    public AuthorViewHolder(View itemView) {
+        super(itemView);
+    }
+}
